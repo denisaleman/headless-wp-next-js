2e66f4a0ad80ba6e3fc235e606d9bde25e137431
Denis Alemán <denis@denisaleman.com>
2026-06-16 21:49:18 +0100

    refactor: menu rest api controller
    - Add MenuModel with built-in transient caching for menu data

---
+++ wordpress/web/app/themes/headless-theme/src/Menu/MenuModel.php
@@ -0,0 +1,87 @@
+<?php
+namespace Gafotas\HeadlessNewsTheme\Menu;
+
+class MenuModel {
+    protected $ttl = 3600; // Cache time-to-live in seconds (1 hour)
+
+    /**
+     * Get menu data for a location, with caching.
+     *
+     * @param string $location
+     * @return array
+     */
+    public function get_menu_by_location( $location ) {
+        $cache_key = 'menu_' . $location;
+        $cached = get_transient( $cache_key );
+        if ( false !== $cached ) {
+            return $cached;
+        }
+
+        $data = $this->build_menu_tree( $location );
+        set_transient( $cache_key, $data, $this->ttl );
+        return $data;
+    }
+
+    /**
+     * Build the hierarchical menu tree for a given location (uncached).
+     *
+     * @param string $location
+     * @return array
+     */
+    private function build_menu_tree( $location ) {
+        $locations = get_nav_menu_locations();
+        if ( ! isset( $locations[ $location ] ) ) {
+            return [];
+        }
+
+        $menu_id = $locations[ $location ];
+        $menu_items = wp_get_nav_menu_items( $menu_id );
+        if ( ! $menu_items ) {
+            return [];
+        }
+
+        $items_by_id = [];
+        foreach ( $menu_items as $item ) {
+            $items_by_id[ $item->ID ] = [
+                'id'       => $item->ID,
+                'title'    => $item->title,
+                'url'      => $item->url,
+                'parent'   => $item->menu_item_parent,
+                'classes'  => implode( ' ', $item->classes ),
+                'target'   => $item->target,
+                'children' => [],
+            ];
+        }
+
+        $tree = [];
+        foreach ( $items_by_id as $id => &$item ) {
+            if ( $item['parent'] == 0 ) {
+                $tree[] = &$item;
+            } else {
+                if ( isset( $items_by_id[ $item['parent'] ] ) ) {
+                    $items_by_id[ $item['parent'] ]['children'][] = &$item;
+                }
+            }
+        }
+        return $tree;
+    }
+
+    /**
+     * Clear the cache for a specific location.
+     *
+     * @param string $location
+     */
+    public function clear_cache( $location ) {
+        delete_transient( 'menu_' . $location );
+    }
+
+    /**
+     * Clear all cached menus (by looping over registered locations).
+     */
+    public function clear_all_cache() {
+        $locations = get_nav_menu_locations();
+        foreach ( array_keys( $locations ) as $location ) {
+            $this->clear_cache( $location );
+        }
+    }
+}
--- wordpress/web/app/themes/headless-theme/src/REST/Controllers/MenuController.php
+++ wordpress/web/app/themes/headless-theme/src/REST/Controllers/MenuController.php
@@ -1,7 +1,15 @@
 <?php
 namespace Gafotas\HeadlessNewsTheme\REST\Controllers;
 
+use Gafotas\HeadlessNewsTheme\Menu\MenuModel;
+
 class MenuController {
+    private $menu_model;
+
+    public function __construct() {
+        $this->menu_model = new MenuModel();
+    }
+
     public function register() {
         add_action( 'rest_api_init', [ $this, 'register_routes' ] );
     }
@@ -9,7 +17,7 @@ class MenuController {
     public function register_routes() {
         register_rest_route( 'headless-news/v1', '/menu/(?P<location>[a-zA-Z0-9_-]+)', [
             'methods'             => 'GET',
-            'callback'            => [ $this, 'get_menu_by_location' ],
+            'callback'            => [ $this, 'get_menu' ],
             'permission_callback' => '__return_true',
             'args'                => [
                 'location' => [
@@ -24,46 +32,9 @@ class MenuController {
         return is_string( $param ) && ! empty( $param );
     }
 
-    public function get_menu_by_location( $request ) {
+	public function get_menu( $request ) {
         $location = $request['location'];
-        $locations = get_nav_menu_locations();
-
-        if ( ! isset( $locations[ $location ] ) ) {
-            return new \WP_REST_Response( [], 200 );
-        }
-
-        $menu_id = $locations[ $location ];
-        $menu_items = wp_get_nav_menu_items( $menu_id );
-
-        if ( ! $menu_items ) {
-            return new \WP_REST_Response( [], 200 );
-        }
-
-        // Build hierarchical tree
-        $items_by_id = [];
-        foreach ( $menu_items as $item ) {
-            $items_by_id[ $item->ID ] = [
-                'id'       => $item->ID,
-                'title'    => $item->title,
-                'url'      => $item->url,
-                'parent'   => $item->menu_item_parent,
-                'classes'  => implode( ' ', $item->classes ),
-                'target'   => $item->target,
-                'children' => [],
-            ];
-        }
-
-        $tree = [];
-        foreach ( $items_by_id as $id => &$item ) {
-            if ( $item['parent'] == 0 ) {
-                $tree[] = &$item;
-            } else {
-                if ( isset( $items_by_id[ $item['parent'] ] ) ) {
-                    $items_by_id[ $item['parent'] ]['children'][] = &$item;
-                }
-            }
-        }
-
-        return rest_ensure_response( $tree );
+        $data = $this->menu_model->get_menu_by_location( $location );
+        return rest_ensure_response( $data );
     }
 }

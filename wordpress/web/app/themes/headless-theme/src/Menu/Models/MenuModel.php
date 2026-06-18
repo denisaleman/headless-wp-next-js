0352c486188963df9c18dafb6d17f04b4e0ba519
Denis Alemán <denis@denisaleman.com>
2026-06-18 22:15:29 +0100

    feat: change menu model
    - now includes name, location and items

--- wordpress/web/app/themes/headless-theme/src/Menu/Models/MenuModel.php
+++ wordpress/web/app/themes/headless-theme/src/Menu/Models/MenuModel.php
@@ -3,7 +3,7 @@ namespace Gafotas\HeadlessNewsTheme\Menu\Models;
 
 class MenuModel {
 	/**
-	 * Build the menu tree for a given location (no cache).
+	 * Get menu data for a given location.
 	 *
 	 * @param string $location
 	 * @return array
@@ -11,13 +11,30 @@ class MenuModel {
 	public function get_menu_by_location( $location ) {
 		$locations = get_nav_menu_locations();
 		if ( ! isset( $locations[ $location ] ) ) {
-			return [];
+			return [
+				'name'     => '',
+				'location' => $location,
+				'items'    => [],
+			];
 		}
 
 		$menu_id = $locations[ $location ];
+		$menu = wp_get_nav_menu_object( $menu_id );
+		if ( ! $menu ) {
+			return [
+				'name'     => '',
+				'location' => $location,
+				'items'    => [],
+			];
+		}
+
 		$menu_items = wp_get_nav_menu_items( $menu_id );
 		if ( ! $menu_items ) {
-			return [];
+			return [
+				'name'     => $menu->name,
+				'location' => $location,
+				'items'    => [],
+			];
 		}
 
 		$items = [];
@@ -31,6 +48,28 @@ class MenuModel {
 				'target'  => $item->target,
 			];
 		}
-		return $items;
+
+		return [
+			'name'     => $menu->name,
+			'location' => $location,
+			'items'    => $items,
+		];
+	}
+
+	/**
+	 * Get multiple menus in one call.
+	 *
+	 * @param string $locations_string Comma-separated list of locations.
+	 * @return array
+	 */
+	public function get_menus_by_locations( $locations_string ) {
+		$locations = array_map( 'trim', explode( ',', $locations_string ) );
+		$result = [];
+		foreach ( $locations as $location ) {
+			if ( ! empty( $location ) ) {
+				$result[ $location ] = $this->get_menu_by_location( $location );
+			}
+		}
+		return $result;
 	}
 }

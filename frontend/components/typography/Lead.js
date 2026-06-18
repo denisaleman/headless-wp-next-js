ad4c13a38a44c69ec5915571b44d2f41ff4aadec
Denis Alemán <denis@denisaleman.com>
2026-06-18 19:19:00 +0100

    feat: add a bit of extra line-height to lead

--- frontend/components/typography/Lead.js
+++ frontend/components/typography/Lead.js
@@ -8,8 +8,9 @@ export default function Lead({ children, className = '', additionalClasses = [] 
       <style jsx>{`
         .lead {
           font-size: 1.70rem;
+          line-height: 1.25em;
           max-width: 65ch;
-          margin: 0;
+          margin: 0 0 1.25rem;
         }
       `}</style>
     </div>
